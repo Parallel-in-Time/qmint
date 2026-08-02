f492e3eab2c398859abc061e65574715e19a5e31
Thibaut Lunet <thibaut.lunet@tuhh.de>
2026-08-02 23:46:52 +0200

    TL: tentative fix for pygments

--- docs/conf.py
+++ docs/conf.py
@@ -112,8 +112,7 @@ language = 'en'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-# pygments_style = "solarized-light"
-# pygments_dark_style = "solarized-dark"
+
 
 copybutton_exclude = ".linenos, .gp"
 copybutton_selector = ":not(.prompt) > div.highlight pre"
@@ -131,6 +130,8 @@ html_theme_options = {
     "use_edit_page_button": True,
     'globaltoc_maxdepth': 2,
     "primary_sidebar_end": ["sidebar-ethical-ads"],
+    "pygments_light_style": "solarized-light",
+    "pygments_dark_style": "solarized-dark",
 }
 
 html_context = {
